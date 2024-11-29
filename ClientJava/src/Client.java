819e8df3f4fbb0776f34e8d9eb167260daaf9e63
Riky-hub <s8289442o@majorana-giorgi.edu.it>
2024-11-29 11:21:36 +0100

    inputStringaQualcosa
    ciao

--- ClientJava/src/Client.java
+++ ClientJava/src/Client.java
@@ -1,23 +1,30 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 public class Client {
 
 	public static void main(String[] args) {
-		String serverAddress = "127.0.0.1"; // Indirizzo IP del server
+		String serverAddress = "10.130.1.127"; // Indirizzo IP del server
         int port = 12345;                  // Porta del server
-
+        
+       
         try (Socket socket = new Socket(serverAddress, port)) {
             System.out.println("Connessione al server " + serverAddress + " sulla porta " + port);
+            
+            
+            Scanner scanner = new Scanner(System.in);
+            String nome = scanner.nextLine();
+           
 
             // Stream di input/output per comunicare con il server
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
 
             InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input)); //converte in caratteri leggibili
 
             // Messaggio da inviare al server
-            String message = "Ciao dal client!";
+            String message = nome;
             writer.println(message);
             System.out.println("Inviato al server: " + message);
 
